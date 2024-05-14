69924b51086cf50a4084e161a0eb1bf4007276e5
LB-Brandon <rmsdud623@gmail.com>
2024-05-14 15:03:41 +0900

    ViewModel 초기화 시점과 사용 시점 변경

--- SelectSaveImagePicker/src/main/java/com/opensource/selectsaveimagepicker/SelectSaveImagePicker.kt
+++ SelectSaveImagePicker/src/main/java/com/opensource/selectsaveimagepicker/SelectSaveImagePicker.kt
@@ -10,16 +10,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.opensource.selectsaveimagepicker.adapter.ImagePickerAdapter
 import com.opensource.selectsaveimagepicker.databinding.FragmentSelectSaveImagePickerBinding
+import com.opensource.selectsaveimagepicker.repository.ImageRepository
 import com.opensource.selectsaveimagepicker.utils.PermissionRequester
+import com.opensource.selectsaveimagepicker.viewmodel.SelectSaveImagePickerViewModel
+import com.opensource.selectsaveimagepicker.viewmodel.ViewModelFactory
+import kotlinx.coroutines.launch
 
 class SelectSaveImagePicker : BottomSheetDialogFragment() {
 	
 	private var _binding: FragmentSelectSaveImagePickerBinding? = null
 	private val binding get() = _binding!!
-	
+	private lateinit var imagePickerAdapter: ImagePickerAdapter
 	private lateinit var permissionRequester: PermissionRequester
+	private val viewModel: SelectSaveImagePickerViewModel by viewModels {
+		ViewModelFactory(ImageRepository(requireContext()))
+	}
 	
 	override fun onAttach(context: Context) {
 		super.onAttach(context)
@@ -31,7 +42,11 @@ class SelectSaveImagePicker : BottomSheetDialogFragment() {
 		container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View {
-		_binding = FragmentSelectSaveImagePickerBinding.inflate(inflater, container, false)
+		_binding = FragmentSelectSaveImagePickerBinding.inflate(
+			inflater,
+			container,
+			false
+		)
 		return binding.root
 	}
 	
@@ -39,10 +54,34 @@ class SelectSaveImagePicker : BottomSheetDialogFragment() {
 		view: View,
 		savedInstanceState: Bundle?
 	) {
-		super.onViewCreated(view, savedInstanceState)
+		super.onViewCreated(
+			view,
+			savedInstanceState
+		)
 		
 		setupPermissionHandling()
+		initView()
 		initListener()
+		initViewModel()
+	}
+	
+	private fun initViewModel() {
+		lifecycleScope.launch {
+			viewModel.images.collect { images ->
+				imagePickerAdapter.submitList(images.toList())
+			}
+		}
+	}
+	
+	private fun initView() {
+		setupImagePickerAdapter()
+	}
+	
+	private fun setupImagePickerAdapter() = with(binding) {
+		imagePickerAdapter = ImagePickerAdapter(viewModel::toggleImageSelection)
+		rvImages.layoutManager = LinearLayoutManager(context)
+		rvImages.adapter = imagePickerAdapter
+		binding.rvImages.setHasFixedSize(true)
 	}
 	
 	private fun initListener() {
@@ -54,10 +93,16 @@ class SelectSaveImagePicker : BottomSheetDialogFragment() {
 	private fun setupPermissionHandling() {
 		val permission = getRequiredPermission()
 		
-		permissionRequester = PermissionRequester(this, permission)
+		permissionRequester = PermissionRequester(
+			this,
+			permission
+		)
 		permissionRequester.onPermissionChecked = {
 			updateViewVisibility(
-				ContextCompat.checkSelfPermission(requireContext(), permission) == PackageManager.PERMISSION_GRANTED
+				ContextCompat.checkSelfPermission(
+					requireContext(),
+					permission
+				) == PackageManager.PERMISSION_GRANTED
 			)
 		}
 	}
@@ -68,15 +113,17 @@ class SelectSaveImagePicker : BottomSheetDialogFragment() {
 	}
 	
 	override fun onResume() {
-		super.onResume()
-		// Recheck permission when fragment resumes
+		super.onResume()		// Recheck permission when fragment resumes
 		checkPermissionsAndUpdateVisibility()
 	}
 	
 	private fun checkPermissionsAndUpdateVisibility() {
 		val permission = getRequiredPermission()
 		updateViewVisibility(
-			ContextCompat.checkSelfPermission(requireContext(), permission) == PackageManager.PERMISSION_GRANTED
+			ContextCompat.checkSelfPermission(
+				requireContext(),
+				permission
+			) == PackageManager.PERMISSION_GRANTED
 		)
 	}
 	
