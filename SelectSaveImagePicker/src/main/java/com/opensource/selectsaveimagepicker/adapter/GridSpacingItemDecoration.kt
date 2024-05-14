407d5915cec814a7af9c13fa0f5dfd995ecba525
LB-Brandon <rmsdud623@gmail.com>
2024-05-14 17:25:23 +0900

    RV 데커레이션 수정

--- SelectSaveImagePicker/src/main/java/com/opensource/selectsaveimagepicker/adapter/GridSpacingItemDecoration.kt
+++ SelectSaveImagePicker/src/main/java/com/opensource/selectsaveimagepicker/adapter/GridSpacingItemDecoration.kt
@@ -17,17 +17,12 @@ class GridSpacingItemDecoration(
 	) {
 		val position = parent.getChildAdapterPosition(view) // 아이템 위치
 		val column = position % spanCount // 현재 위치의 열 인덱스
-		val itemCount = state.itemCount // 아이템 총 개수
-		val extraBottomSpacing = 500 // 마지막 줄 바닥 여백 추가
 		
 		outRect.left = column * spacing / spanCount
 		outRect.right = spacing - (column + 1) * spacing / spanCount
 		
 		if (position >= spanCount) {
 			outRect.top = spacing
-		} // 마지막 줄의 경우에만 추가 바닥 여백 적용
-		if (position == itemCount) {
-			outRect.bottom = extraBottomSpacing
 		}
 	}
 }
